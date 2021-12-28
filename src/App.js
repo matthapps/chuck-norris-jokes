ba7a14ee10c6338f168e20c6c512c19dca5fe210
matthapps <mhheinrichs@gmail.com>
2021-12-28 09:26:29 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect} from 'react'
 
 function App() {
+
+  const [data, setData] = useState();
+
+  const apiJoke = () => {
+    fetch("https://api.chucknorris.io/jokes/random")
+      .then((res) => res.json())
+      .then((json) => {
+        setData(json.value)
+      });
+  };
+
+  // generates quote onload
+  useEffect(() => {
+    apiJoke()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1 className='App-title'>Chuck Norris Random Jokes</h1>
+      <img
+        className='App-img'
+        src='https://cdn.flickeringmyth.com/wp-content/uploads/2020/03/chuck-norris.jpg'
+        alt='Chuck Norris'
+      ></img>
+      <button onClick={apiJoke} className='App-btn'>
+        Click for new Chuck Norris Joke
+      </button>
+      <h4 className='App-joke-text'>{JSON.stringify(data)}</h4>
     </div>
   );
 }
